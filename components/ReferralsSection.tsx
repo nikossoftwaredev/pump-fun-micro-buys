68b3cb1ab8f479702637ccae2bfa011896d9a3fc
Nikos Dimitrakopoulos <nikossoftwaredev@gmail.com>
2024-06-25 10:51:17 +0300

    fdsfdfd

--- components/ReferralsSection.tsx
+++ components/ReferralsSection.tsx
@@ -4,6 +4,8 @@ import { TOKEN_SYMBOL } from "@/data/config";
 import axios from "axios";
 import UserRow from "./UserRow";
 
+export const revalidate = 60
+
 const ReferralsSection = async () => {
   const { data } = await axios.get(
     `http://34.171.163.77:8080/referrals`
