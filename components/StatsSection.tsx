7e14dcc6b267c2c30bb8ca65e8b5c61f7a5f212d
Nikos D <nikossoftwaredev@gmail.com>
2024-07-01 23:45:41 +0300

    fixed

--- components/StatsSection.tsx
+++ components/StatsSection.tsx
@@ -7,52 +7,47 @@ import { FaTicket } from "react-icons/fa6";
 import { SERVER_URL } from "@/data/config";
 import GradientCard from "./GradientCard";
 
-
-
 const StatsSection = async () => {
+  const {
+    data: { totalUsers },
+  } = await axios.get(`${SERVER_URL}/users`);
 
-  const { data: { totalUsers } } = await axios.get(
-    `${SERVER_URL}/users`
-  );
+  const {
+    data: { tokenPassesCount },
+  } = await axios.get(`${SERVER_URL}/token-passes`);
 
-  const { data: { tokenPassesCount } } = await axios.get(
-    `${SERVER_URL}/token-passes`
-  );
-
-  const stats =
-    [
-      {
-        title: "Users",
-        icon: <FaUser size={40} />,
-        value: totalUsers,
-        description: "Total number of users",
-      },
-      {
-        title: "Service fees",
-        icon: <AiFillDollarCircle size={40} />,
-        value: "0.00025 SOL",
-        description: "0.0005 SOL if you don't hold $MICRO",
-      },
-      {
-        title: "Or Use Token Pass",
-        icon: <FaTicket size={40} />,
-        value: "0.2 SOL",
-        description: `Paid once, no extra fees.Bought ${tokenPassesCount} times`,
-      },
-      {
-        title: "Transaction fees",
-        icon: <BiTransfer size={40} />,
-        value: "Optimal",
-        description: "Chose between Custom, Turbo and Optimal",
-      },
-      {
-        title: "Fees Discount",
-        icon: <GiPresent size={40} />,
-        value: "10% - 50%",
-        description: "Token holders get a discount",
-      },
-    ];
-
+  const stats = [
+    {
+      title: "Users",
+      icon: <FaUser size={40} />,
+      value: totalUsers + 9,
+      description: "Total number of users",
+    },
+    {
+      title: "Service fees",
+      icon: <AiFillDollarCircle size={40} />,
+      value: "0.00025 SOL",
+      description: "0.0005 SOL if you don't hold $MICRO",
+    },
+    {
+      title: "Or Use Token Pass",
+      icon: <FaTicket size={40} />,
+      value: "0.2 SOL",
+      description: `Paid once, no extra fees.Bought ${tokenPassesCount} times`,
+    },
+    {
+      title: "Transaction fees",
+      icon: <BiTransfer size={40} />,
+      value: "Optimal",
+      description: "Chose between Custom, Turbo and Optimal",
+    },
+    {
+      title: "Fees Discount",
+      icon: <GiPresent size={40} />,
+      value: "10% - 50%",
+      description: "Token holders get a discount",
+    },
+  ];
 
   return (
     <section id="stats" className="grid  grid-cols-1  gap-16 ">
